d8dc554fe2b9bfbe21f621a1b994f6eae11f3ace
DanielJPiazza <DanielJPiazza@users.noreply.github.com>
2019-08-26 17:36:44 -0400

    Finish implementation of basic main menu.
    - Organize code, create constants for common values, & add more comments.
    - Create reset_game() function that preps for a new game (ex. reset ball/paddle positions).
    - Fix bug where main menu's green (new game) button could only be used once without restarting the program.
    - Fixed main menu button positions.

--- pong.py
+++ pong.py
@@ -1,11 +1,12 @@
-# TUTORIAL - https://nick.sarbicki.com/blog/learn-pygame-with-pong/
+# BASE TUTORIAL - https://nick.sarbicki.com/blog/learn-pygame-with-pong/
 
 # IMPORTS
 import pygame
+import os
 from random import randint
 from sys import exit
 
-# PADDLE CLASS
+# PLAYER PADDLE
 class Paddle(pygame.Rect):
 	def __init__(self, velocity, up_key, down_key, *args, **kwargs):
 		self.velocity = velocity
@@ -24,7 +25,7 @@ class Paddle(pygame.Rect):
 			if self.y + self.velocity < board_height - self.height:
 				self.y += self.velocity
 		
-# BALL CLASS
+# BALL
 class Ball(pygame.Rect):
 	def __init__(self, velocity, *args, **kwargs):
 		self.velocity = velocity
@@ -37,26 +38,59 @@ class Ball(pygame.Rect):
 
 # MAIN
 class Pong:
-	HEIGHT = 800
-	WIDTH = 1600
-	
-	PADDLE_WIDTH = 10
-	PADDLE_HEIGHT = 100
-	
-	BALL_WIDTH = 10
-	BALL_VELOCITY = 10
-	
-	COLOR = (255, 255, 255)
-	
 	def __init__(self):
+		# GAME & MAIN MENU VARIABLES
+		self.WINDOW_HEIGHT_OFFSET = 200
+		self.WINDOW_WIDTH_OFFSET = 300
+
+		self.WINDOW_X = 40
+		self.WINDOW_Y = 60
+		
+		self.PADDLE_WIDTH = 10
+		self.PADDLE_HEIGHT = 100
+		
+		self.BALL_WIDTH = 10
+		self.BALL_VELOCITY = 10
+		
+		self.WHITE = (255, 255, 255)
+		self.BLACK = (0, 0, 0)
+
+		self.MENU_BUTTON_WIDTH = 100
+		self.MENU_BUTTON_HEIGHT = 50
+		self.MENU_GREEN = (0, 255, 0)
+		self.MENU_RED = (255, 0, 0)
+		
 		# Start Pygame instance.
 		pygame.init()
 		
 		# Screen setup.
-		self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-		self.clock = pygame.time.Clock()
-		print("\nWelcome to Pygame & Pong!")
+		pygame.display.set_caption("Pygame Pong")										# Window caption.
+		os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (self.WINDOW_X, self.WINDOW_Y)	# Initial window position.
+		self.DISPLAY_INFO = pygame.display.Info()										# Get window info.
+		self.HEIGHT = self.DISPLAY_INFO.current_h - self.WINDOW_HEIGHT_OFFSET			# Screen height.
+		self.WIDTH = self.DISPLAY_INFO.current_w - self.WINDOW_WIDTH_OFFSET				# Screen width.
+		self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))				# Set display mode,
+		self.clock = pygame.time.Clock()												# Create clock.
 		
+	# Check if the ball has collided with a wall.
+	def check_ball_hits_wall(self):
+		for ball in self.balls:
+			if ball.x > self.WIDTH or ball.x <0:
+				self.main_menu()
+			if ball.y > self.HEIGHT - self.BALL_WIDTH or ball.y <0:
+				ball.angle = -ball.angle
+				
+	# Check if the ball has collided with either paddle.
+	def check_ball_hits_paddle(self):
+		for ball in self.balls:
+			for paddle in self.paddles:
+				if ball.colliderect(paddle):
+					ball.velocity = -ball.velocity
+					ball.angle = randint(-10, 10)
+					break
+	
+	# Start a new game via main menu button.
+	def reset_game(self):
 		# Create player objects.
 		self.paddles = []
 		self.balls = []
@@ -89,50 +123,84 @@ class Pong:
 			self.BALL_WIDTH
 		))
 		
-		# Add "net" to middle of screen.
+		# Add net to middle of screen.
 		self.central_line = pygame.Rect(self.WIDTH/2, 0, 1, self.HEIGHT)
-		
-	def check_ball_hits_wall(self):
-		for ball in self.balls:
-			if ball.x > self.WIDTH or ball.x <0:
-				print("\nThe ball got by a paddle - GAME OVER!")
-				exit(1)
-			if ball.y > self.HEIGHT - self.BALL_WIDTH or ball.y <0:
-				ball.angle = -ball.angle
-				
-	def check_ball_hits_paddle(self):
-		for ball in self.balls:
-			for paddle in self.paddles:
-				if ball.colliderect(paddle):
-					ball.velocity = -ball.velocity
-					ball.angle = randint(-10, 10)
-					break
 	
+	# Main menu - allows users to play or exit the program.
+	def main_menu(self):
+		while True:			
+			# Redraw the screen.
+			self.screen.fill(self.BLACK)
+			
+			# Menu buttons
+			green_button = pygame.draw.rect(
+				self.screen,
+				self.MENU_GREEN, (
+					(self.WIDTH/2)-(self.MENU_BUTTON_WIDTH/2),
+					self.HEIGHT - (self.MENU_BUTTON_WIDTH + (self.MENU_BUTTON_WIDTH * 0.75)),
+					self.MENU_BUTTON_WIDTH,
+					self.MENU_BUTTON_HEIGHT
+				)
+			)
+
+			red_button = pygame.draw.rect(
+				self.screen,
+				self.MENU_RED, (
+					(self.WIDTH/2) - (self.MENU_BUTTON_WIDTH/2),
+					self.HEIGHT - self.MENU_BUTTON_WIDTH,
+					self.MENU_BUTTON_WIDTH,
+					self.MENU_BUTTON_HEIGHT
+				)
+			)
+
+			# Check for mouse clicks on menu and OS close window buttons.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					exit(0)			
+			
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					# Set the x, y postions of the mouse click.
+					x, y = event.pos
+					# START GAME
+					if green_button.collidepoint(x, y):
+						self.reset_game()
+						self.game_loop()
+					# EXIT GAME
+					if red_button.collidepoint(x, y):
+						pygame.quit()
+						exit(0)						
+
+			# Flip drawn graphics to screen, and manage the clock.
+			pygame.display.flip()
+			self.clock.tick(60)
+	
+	# Main game loop.
 	def game_loop(self):
 		while True:
 			# Exit by clicking windows' close button.
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
-					exit()
+					exit(0)
 					
+			# Redraw the screen.
+			self.screen.fill(self.BLACK)			
+			
 			# Check for ball collisions.
 			self.check_ball_hits_paddle()
 			self.check_ball_hits_wall()
-					
-			# Redraw the screen.
-			self.screen.fill((0, 0, 0))
 			
 			# MOVE & DRAW
-			pygame.draw.rect(self.screen, self.COLOR, self.central_line)
+			pygame.draw.rect(self.screen, self.WHITE, self.central_line)	# NET
 			
-			for paddle in self.paddles:
+			for paddle in self.paddles:										# PADDLES
 				paddle.move_paddle(self.HEIGHT)
-				pygame.draw.rect(self.screen, self.COLOR, paddle)
+				pygame.draw.rect(self.screen, self.WHITE, paddle)
 				
-			for ball in self.balls:
+			for ball in self.balls:											# BALL
 				ball.move_ball()
-				pygame.draw.rect(self.screen, self.COLOR, ball)
+				pygame.draw.rect(self.screen, self.WHITE, ball)
 				
 			# Flip drawn graphics to screen, and manage the clock.
 			pygame.display.flip()
@@ -141,4 +209,4 @@ class Pong:
 # START
 if __name__ == '__main__':
 	pong = Pong()
-	pong.game_loop()
+	pong.main_menu()
